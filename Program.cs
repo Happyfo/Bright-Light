ec1ccb9cea53f379cddaa62cd957a5d946d48cb2
Happiness Rose Mary <happinessfofo1997@gmail.com>
2018-12-04 14:19:34 +0200

    added assignment

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HaAgy
+{
+    class Calculate
+    {
+        static void Main(string[] args)
+        {
+            var operation = new Calculate();
+
+            Console.WriteLine("please enter first number");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("please enter second number");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("enter operation");
+        }
+
+        internal object Add(double num1, double num2)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal object multiplication(double num1, double num2)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal object Subtraction(double num1, double num2)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal object division(double num1, double num2)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal object Multiplication(double num1, double num2)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal object Division(double num1, double num2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ calculator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HaAgy
+{
+    class calculator
+    {
+        static void main(string[] args)
+        {
+            int op;
+            Console.WriteLine("Operations");
+            Console.WriteLine("1.Addition");
+            Console.WriteLine("2.Subtraction");
+            Console.WriteLine("3.Multiplication");
+            Console.WriteLine("4.Division");
+            op = Convert.ToInt32(Console.ReadLine());
+            Console.ReadKey();
+
+            Console.WriteLine("Enter first number");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Second number");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            
+            var result = new Calculate();
+            switch(op)
+            {
+                case 1:
+                Console.WriteLine("the sum of two  numbers {0} and {1} =  {2}",num1, num2, result.Add(num1,num2));
+                Console.ReadKey();
+                break;
+                case 2:
+                Console.WriteLine("the difference of two numbers {0} and {1} = {2}", num1, num2, result.Subtraction(num1, num2));
+                Console.ReadKey();
+                break;
+                case 3:
+                Console.WriteLine("the product of two numbers {0} and {1} = {2}", num1, num2, result.Multiplication(num1, num2));
+                Console.ReadKey();
+                break;
+                case 4:
+                Console.WriteLine("the quotient of two numbers {0} and {1} = {2}", num1, num2, result.Division(num1, num2));
+                Console.ReadKey();
+                break;
+                default:
+                Console.WriteLine("operand not specified");
+                Console.ReadKey();
+                break;
+            }
+        }
+    }
+}
